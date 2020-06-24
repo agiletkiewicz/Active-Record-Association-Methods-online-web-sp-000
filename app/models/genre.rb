befa89f10721b727b7ea3bdc16afe46d9c9a4d39
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-06-24 23:18:11 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -7,14 +7,7 @@ class Genre < ActiveRecord::Base
   end
 
   def artist_count
-      artist_array =[]
-      self.songs.each do |song|
-      if artist_array.include?(song.artist)
-      else 
-        artist_array << song.artist
-      end
-    end
-    artist_array.count
+    artists.count
   end
 
   def all_artist_names
